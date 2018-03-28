c1450bb3bc9845a2acf68a7e0c1acdf1b5da4746
Diego Araujo <diego_shakan@hotmail.com>
2018-03-28 20:30:11 -0300

    Observações para fazer funcionar este programa.

--- Desafio21.py
+++ Desafio21.py
@@ -9,3 +9,6 @@ pygame.event.wait()
 
 '''É necessário instalar o pacote do pygame, não é o foco do curso neste momento
 como dito pelo prof Gustavo Guanabara no desafio.'''
+
+'''É necessário que o arquivo de áudio esteja no mesmo projeto que vocẽ está fazendo o programa, 
+na mesma pasta. Não estou commitando o .mp3 porque a música fica a sua escolha.'''

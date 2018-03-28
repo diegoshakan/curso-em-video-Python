f1a7609917781712a599a51b7f665140405c35ac
Diego Araujo <diego_shakan@hotmail.com>
2018-03-28 20:27:06 -0300

    Acrescentei os parênteses que tinha esquecido.

--- Desafio21.py
+++ Desafio21.py
@@ -3,9 +3,9 @@ import pygame
 '''Faça um programa que seja capaz de tocar um mp3'''
 
 pygame.init()
-pygame.mixer.music.load('Desafio21.mp3')
-pygame.mixer.music.play
-pygame.event.wait
+pygame.mixer.music.load('oi.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
 
 '''É necessário instalar o pacote do pygame, não é o foco do curso neste momento
 como dito pelo prof Gustavo Guanabara no desafio.'''
